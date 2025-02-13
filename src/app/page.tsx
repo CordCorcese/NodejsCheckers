676d0ba5512d506da2b93bd3922d701abb893d34
Cord Corcese <badwithnames14@gmail.com>
2025-02-13 02:52:35 -0800

    Created basic board layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,87 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         CHECKERS
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-         
+        <div className="board">
+          <div className="boardRow">
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          </div>
+          <div className="boardRow">
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          </div>
+          <div className="boardRow">
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          </div>
+          <div className="boardRow">
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          </div>
+          <div className="boardRow">
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          </div>
+          <div className="boardRow">
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          </div>
+          <div className="boardRow">
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          </div>
+          <div className="boardRow">
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          <button className="boardSquare"></button>
+          </div>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
